e021005789711912696f278aa5d08946927fbb6e
Shayan <Shayan.aryan.sh@gmail.com>
2016-07-31 17:51:12 +0430

    clean up

--- app/src/main/java/com/shayan/booking/helper/recyclerview/GridAutoFitLayoutManager.java
+++ app/src/main/java/com/shayan/booking/helper/recyclerview/GridAutoFitLayoutManager.java
@@ -8,28 +8,24 @@ import android.util.TypedValue;
 /**
  * Created by Shayan on 7/27/2016.
  */
-public class GridAutoFitLayoutManager extends GridLayoutManager{
+public class GridAutoFitLayoutManager extends GridLayoutManager {
     private int mColumnWidth;
     private boolean mColumnWidthChanged = true;
 
-    public GridAutoFitLayoutManager(Context context, int columnWidth)
-    {
+    public GridAutoFitLayoutManager(Context context, int columnWidth) {
         /* Initially set spanCount to 1, will be changed automatically later. */
         super(context, 1);
         setColumnWidth(checkedColumnWidth(context, columnWidth));
     }
 
-    public GridAutoFitLayoutManager(Context context, int columnWidth, int orientation, boolean reverseLayout)
-    {
+    public GridAutoFitLayoutManager(Context context, int columnWidth, int orientation, boolean reverseLayout) {
         /* Initially set spanCount to 1, will be changed automatically later. */
         super(context, 1, orientation, reverseLayout);
         setColumnWidth(checkedColumnWidth(context, columnWidth));
     }
 
-    private int checkedColumnWidth(Context context, int columnWidth)
-    {
-        if (columnWidth <= 0)
-        {
+    private int checkedColumnWidth(Context context, int columnWidth) {
+        if (columnWidth <= 0) {
             /* Set default columnWidth value (48dp here). It is better to move this constant
             to static constant on top, but we need context to convert it to dp, so can't really
             do so. */
@@ -39,29 +35,22 @@ public class GridAutoFitLayoutManager extends GridLayoutManager{
         return columnWidth;
     }
 
-    public void setColumnWidth(int newColumnWidth)
-    {
-        if (newColumnWidth > 0 && newColumnWidth != mColumnWidth)
-        {
+    public void setColumnWidth(int newColumnWidth) {
+        if (newColumnWidth > 0 && newColumnWidth != mColumnWidth) {
             mColumnWidth = newColumnWidth;
             mColumnWidthChanged = true;
         }
     }
 
     @Override
-    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state)
-    {
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
         int width = getWidth();
         int height = getHeight();
-        if (mColumnWidthChanged && mColumnWidth > 0 && width > 0 && height > 0)
-        {
+        if (mColumnWidthChanged && mColumnWidth > 0 && width > 0 && height > 0) {
             int totalSpace;
-            if (getOrientation() == VERTICAL)
-            {
+            if (getOrientation() == VERTICAL) {
                 totalSpace = width - getPaddingRight() - getPaddingLeft();
-            }
-            else
-            {
+            } else {
                 totalSpace = height - getPaddingTop() - getPaddingBottom();
             }
             int spanCount = Math.max(1, totalSpace / mColumnWidth);
